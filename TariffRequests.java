ce7153dc7fa9556e0bbb3a08ce97567af1696617
Christina <136536472+chrisalexan06@users.noreply.github.com>
2025-05-20 00:18:16 -0400

    Update TariffRequests.java

--- TariffRequests.java
+++ TariffRequests.java
@@ -1,9 +1,3 @@
-
-//--------------------------------------------------------
-// Assignment 3
-// Question: Part 2 linked list (interface)
-// Written by: Christina Alexandrakis 40326572
-//--------------------------------------------------------
 package part2;
 
 public class TariffRequests{
